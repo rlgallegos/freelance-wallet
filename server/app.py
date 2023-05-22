1694577380c68503b7e82167f13197cfdb334c17
Robert <rlgallegos85@gmail.com>
2023-05-22 14:52:45 -0400

    Found path error

--- server/app.py
+++ server/app.py
@@ -131,9 +131,11 @@ api.add_resource(IncomeById, '/incomes/<int:id>')
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def serve_frontend(path):
-    return render_template('index.html')
-
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
