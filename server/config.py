a2b2f62ee93ee1d75b491b5ba501251304e105b2
Robert <rlgallegos85@gmail.com>
2023-05-31 20:08:10 -0400

    samesite

--- server/config.py
+++ server/config.py
@@ -26,8 +26,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///freelance.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SECRET_KEY'] = os.environ.get('FLASK_APP_SECRET_KEY')
 app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-app.config['SESSION_COOKIE_SECURE'] = True
+# app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config['SESSION_COOKIE_SECURE'] = False
 
 CORS(app, origins=['https://freelance-wallet.vercel.app'])
 
