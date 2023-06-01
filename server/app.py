bfdacba8336f9327ed358bba5cc9694349683dc6
Robert <rlgallegos85@gmail.com>
2023-06-01 03:36:25 -0400

    udpate

--- server/app.py
+++ server/app.py
@@ -78,7 +78,9 @@ class Login(Resource):
         print(session)
         print(dict(session))
         response = make_response(user.to_dict(), 200)
-        response.headers['Access-Control-Allow-Credentials'] = True
+        if 'Access-Control-Allow-Credentials' in response.headers:
+            del response.headers['Access-Control-Allow-Credentials']
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
         return response
 
 api.add_resource(Login, '/login')
